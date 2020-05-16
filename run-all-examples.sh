4e47861e53ce671716e8c72d212884e086f4688e
Viktor Chvatal <viktor@chvatal.name>
2020-05-16 15:17:41 +0200

    Dev dependencies optimized, added script to run all examples.

---
+++ run-all-examples.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cargo run --release --example load_save
+cargo run --release --example negative_image
+cargo run --release --example vertical_convolution
+cargo run --release --example horizontal_convolution
